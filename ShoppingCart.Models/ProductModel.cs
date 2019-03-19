9101bbe026ff46d4f49984152a57f544e73a485d
Talha Zengin <talha.zengin@matriksdata.com>
2019-03-19 18:18:40 +0300

    Models project added

---
+++ ShoppingCart.Models/ProductModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ShoppingCart.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+    }
+}
--- ShoppingCart/Startup.cs
+++ ShoppingCart/Startup.cs
@@ -29,7 +29,7 @@ namespace ShoppingCart
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
