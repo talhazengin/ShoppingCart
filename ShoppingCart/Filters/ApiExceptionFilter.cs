17ee3d17008178389bd9970f2b6c23866d618598
Talha Zengin <zengintalha@gmail.com>
2019-04-04 00:34:19 +0300

    little refactor.

--- ShoppingCart/Filters/ApiExceptionFilter.cs
+++ ShoppingCart/Filters/ApiExceptionFilter.cs
@@ -4,7 +4,6 @@ using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
-using ShoppingCart.Core;
 using ShoppingCart.Core.Exceptions;
 
 namespace ShoppingCart.Filters
@@ -13,31 +12,33 @@ namespace ShoppingCart.Filters
     {
         public override void OnException(ExceptionContext context)
         {
-            if (context.Exception is NotFoundException notFoundException)
+            switch (context.Exception)
             {
-                // handle explicit 'known' API errors
-                context.Exception = null;
+                case NotFoundException notFoundException:
+                    // handle explicit 'known' API errors
+                    context.Exception = null;
 
-                context.Result = new JsonResult(notFoundException.Message);
-                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.NotFound;
-            }
-            else if (context.Exception is BadRequestException badRequestException)
-            {
-                // handle explicit 'known' API errors
-                context.Exception = null;
+                    context.Result = new JsonResult(notFoundException.Message);
+                    context.HttpContext.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                    break;
 
-                context.Result = new JsonResult(badRequestException.Message);
-                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-            }
-            else if (context.Exception is UnauthorizedAccessException)
-            {
-                context.Result = new JsonResult(context.Exception.Message);
-                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
-            }
-            else if (context.Exception is ForbiddenException)
-            {
-                context.Result = new JsonResult(context.Exception.Message);
-                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                case BadRequestException badRequestException:
+                    // handle explicit 'known' API errors
+                    context.Exception = null;
+
+                    context.Result = new JsonResult(badRequestException.Message);
+                    context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    break;
+
+                case UnauthorizedAccessException _:
+                    context.Result = new JsonResult(context.Exception.Message);
+                    context.HttpContext.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                    break;
+
+                case ForbiddenException _:
+                    context.Result = new JsonResult(context.Exception.Message);
+                    context.HttpContext.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                    break;
             }
 
             base.OnException(context);
